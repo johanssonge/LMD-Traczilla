b1c619a357adc6a3e542adad5ef7351a141586bc
Erik Johansson <erik.johansson@lmd.ipsl.fr>
2021-10-12 11:22:37 +0200

    Initiate analysing file

---
+++ analyseTraczilla.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+
+Created on 2021-10-04
+
+Copyright (c) 2021 Erik Johansson
+
+
+@author:     Erik Johansson
+@contact: <erik.johansson@lmd.ipsl.fr>
+
+"""
+
+import numpy as np
+
+
+if __name__ == '__main__':
+    pass
