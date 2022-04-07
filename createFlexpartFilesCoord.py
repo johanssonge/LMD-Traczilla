6633e7c1f908b93c42976568ce756a08ff2e5ea5
Erik Johansson <erik.johansson@lmd.ipsl.fr>
2022-04-07 13:50:18 +0200

    Initiate new file

---
+++ createFlexpartFilesCoord.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+
+Created on 2022-04-07
+
+Copyright (c) 2022 Erik Johansson
+
+
+@author:     Erik Johansson
+@contact: <erik.johansson@lmd.ipsl.fr>
+
+"""
+
+import numpy as np
+
+
+if __name__ == '__main__':
+    pass
